329579a0861b2e4bf68dddb9d821095ba9c64a50
can-dy-jack <jjack_chen3@163.com>
2022-05-12 15:55:24 +0800

    update to delicate3!

--- js/index.js
+++ js/index.js
@@ -1,53 +1,5 @@
-/**
- * copy button
- */
-const codeSpans = document.querySelectorAll('figure');
-codeSpans.forEach(function (code) {
-    // 代码块提示信息div
-    const codeInfo = document.createElement('div');
-    codeInfo.classList.add("code-info");
-    // 代码类型
-    const codeName = code.classList[1];
-    const codeNameSpan = document.createElement("span");
-    codeNameSpan.style.color = "#007bff";
-    codeNameSpan.textContent = codeName;
-    codeInfo.appendChild(codeNameSpan);
-    // 复制按钮
-    const codeButton = document.createElement("span");
-
-    codeButton.innerHTML = '<svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="20" height="20"><path d="M853.333333 981.333333h-384c-72.533333 0-128-55.466667-128-128v-384c0-72.533333 55.466667-128 128-128h384c72.533333 0 128 55.466667 128 128v384c0 72.533333-55.466667 128-128 128z m-384-554.666666c-25.6 0-42.666667 17.066667-42.666666 42.666666v384c0 25.6 17.066667 42.666667 42.666666 42.666667h384c25.6 0 42.666667-17.066667 42.666667-42.666667v-384c0-25.6-17.066667-42.666667-42.666667-42.666666h-384z" p-id="2583"></path><path d="M213.333333 682.666667H170.666667c-72.533333 0-128-55.466667-128-128V170.666667c0-72.533333 55.466667-128 128-128h384c72.533333 0 128 55.466667 128 128v42.666666c0 25.6-17.066667 42.666667-42.666667 42.666667s-42.666667-17.066667-42.666667-42.666667V170.666667c0-25.6-17.066667-42.666667-42.666666-42.666667H170.666667c-25.6 0-42.666667 17.066667-42.666667 42.666667v384c0 25.6 17.066667 42.666667 42.666667 42.666666h42.666666c25.6 0 42.666667 17.066667 42.666667 42.666667s-17.066667 42.666667-42.666667 42.666667z"></path></svg>';
-    codeButton.className = "btn-copy";
-    codeButton.setAttribute('data-container', 'body');
-    codeButton.setAttribute('data-toggle', 'popover');
-    codeButton.setAttribute('data-placement', 'top');
-    codeButton.setAttribute('data-content', '复制成功');
-    codeInfo.appendChild(codeButton);
-    2
-
-    code.appendChild(codeInfo);
-
-    codeButton.addEventListener('click', () => {
-        navigator.clipboard.writeText(code.children[0].children[0].children[0].children[1].innerText)
-        setTimeout(() => {
-            $('.btn-copy').popover('hide');
-        }, 2000);
-    });
-})
-
-/**
- *  tooltip
- * 提示框 - bootstrap + popper
- * */
-$('#feedback').tooltip();
-$('.btn-copy').popover();
-$('.icon-box').tooltip();
-$('.origin').tooltip();
-
-/**
- * 网站运行时间计算函数
- */
-var now = new Date();
-
+let now = new Date();
+// 计算网站运算时间的函数
 function calculateTime(startTime) {
     now.setTime(now.getTime() + 1000);
     stime = new Date(startTime);
@@ -66,22 +18,168 @@ function calculateTime(startTime) {
     }
     document.getElementById("times").innerHTML = "本站已运行 " + dnum + " 天 " + hnum + " 小时 " + mnum + " 分，共" + Math.floor(interval / 1000 / 60 / 60) + "小时";
 }
-function douban() {
-    var d = document,
-        e = encodeURIComponent,
-        s1 = window.getSelection,
-        s2 = d.getSelection,
-        s3 = d.selection,
-        s = s1 ? s1() : s2 ? s2() : s3 ? s3.createRange().text : '',
-        r = 'https://www.douban.com/recommend/?url=' + e(d.location.href) + '&title=' + e(d.title) + '&sel=' + e(s) + '&v=1',
-        w = 450,
-        h = 450,
-        x = function () {
-            if (!window.open(r, 'douban', 'toolbar=0,resizable=1,scrollbars=yes,status=1,width=' + w + ',height=' + h + ',left=' + (screen.width - w) / 2 + ',top=' + (screen.height - h) / 2)) location.href = r + '&r=1'
-        };
-    if (/Firefox/.test(navigator.userAgent)) {
-        setTimeout(x, 0)
-    } else {
-        x()
+
+/** 
+ * 打字机效果
+ * 利用js的 split() 函数能更好的把不同文字按照各自语言的标准，将最小字符分开
+ */
+const indexTitleBox = document.getElementById("index-title-box");
+if(indexTitleBox != null){
+    const chars = indexTitleBox.textContent.split('');
+    // 清空文字
+    indexTitleBox.textContent = '';
+    // 将分开后的字符外层加个span标签，再加进去
+    for(let i = 0;i < chars.length;i++){
+        setTimeout(()=>{
+            const span = document.createElement('span');
+            span.classList.add('transition');
+            span.innerText = chars[i];
+            indexTitleBox.appendChild(span);
+        },200*i);
     }
 }
+
+// 滚动监听 + to_top返回顶端
+// toc
+const toc = document.querySelector(".post-toc .toc");
+// h2 tags
+const header2 = document.querySelectorAll('.post>h2');
+// toc tags
+const tocLinks = document.querySelectorAll('.toc>.toc-item>.toc-link');
+// to_top
+const to_top = document.getElementById('to_top');
+
+function clearActive(nodes){
+    nodes.forEach(item => {
+        item.classList.remove("active");
+    });
+}
+if(header2.length){
+    // 获取h2分布范围
+    const header2_ranges = new Array(header2.length).fill(0).map(()=>[]);
+    for(let i = 0;i<header2.length-1;i++){
+        header2_ranges[i].push(header2[i].offsetTop,header2[i+1].offsetTop)
+    }
+    header2_ranges[header2.length-1] = [header2[header2.length-1].offsetTop,Number.MAX_VALUE];
+    // 跳转到h2后，toc样式对应变换
+    function tocTransfrom(){
+        // header2[i].offsetTop | document.documentElement.scrollTop
+        for(let i = 0;i<header2.length;i++){
+            if(document.documentElement.scrollTop > header2_ranges[i][0] && document.documentElement.scrollTop <= header2_ranges[i][1]){
+                clearActive(tocLinks);
+                tocLinks[i].classList.add("active");
+                break;
+            }
+        }
+        // to_top
+        if(scrollY <= screen.availHeight){
+            to_top.style.transform = 'translateY(70px)';
+        }else{
+            to_top.style.transform = 'translateY(0)';
+        }
+    }
+} else {
+    function tocTransfrom(){
+        // to_top
+        if(scrollY <= screen.availHeight){
+            to_top.style.transform = 'translateY(70px)';
+        }else{
+            to_top.style.transform = 'translateY(0)';
+        }
+    }
+}
+to_top.addEventListener('click',()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+    window.onscroll = null;
+    to_top.style.transform = 'translateY(70px)';
+    setTimeout(()=>{
+        window.onscroll = tocTransfrom;
+        tocTransfrom();
+    },1000);
+});
+
+// friends-more
+const friends_more = document.getElementById("friends-more");
+const phone_nav_link = document.getElementById("phone-nav-link");
+let phone_nav_link_is_open = false;
+friends_more.addEventListener('click',()=>{
+    if(phone_nav_link_is_open) {
+        phone_nav_link.style.transform = 'scaleX(0)';
+    } else {
+       phone_nav_link.style.transform = 'scaleX(1)'; 
+    }
+    phone_nav_link_is_open = !phone_nav_link_is_open;
+})
+
+// 移动端toc
+const tocBox = document.getElementById("post-toc");
+const tocBtn = document.getElementById("open-toc");
+if(tocBox !== null && tocBtn !== null) {
+    let toc_open = false;
+    tocBtn.addEventListener('click',()=>{
+        if(toc_open) {
+            tocBox.style.transform = 'scaleY(0)';
+        } else {
+            tocBox.style.transform = 'scaleY(1)';
+        }
+        toc_open = !toc_open;
+    })
+    window.onresize = ()=> {
+        if(window.innerWidth <= 800) { 
+            toc_open = false;
+            tocBox.style.transform = 'scaleY(0)';
+        } else {
+            toc_open = true;
+            tocBox.style.transform = 'scaleY(1)';
+            phone_nav_link.style.transform = 'scaleX(0)';
+        }
+    }
+}
+
+// 为代码块添加 代码信息
+const code =document.querySelectorAll("figure.highlight table tr td.code");
+const code_block =document.querySelectorAll("figure.highlight");
+
+for(let i = 0;i<code_block.length;i++) {
+    const code_info = document.createElement('div');
+    code_info.classList.add('code-info');
+
+    const text =document.createElement("span");
+    text.textContent = code_block[i].classList[1];
+    text.classList.add("code-type");
+
+    const copy_btn =document.createElement("span");
+    const copy_text =document.createElement("span");
+    const copy_svg =document.createElement("span");
+    copy_btn.classList.add("copy-btn");
+    copy_text.innerText = "copy";
+    copy_svg.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="16" height="16"><path d="M0 224C0 188.7 28.65 160 64 160H128V288C128 341 170.1 384 224 384H352V448C352 483.3 323.3 512 288 512H64C28.65 512 0 483.3 0 448V224zM224 352C188.7 352 160 323.3 160 288V64C160 28.65 188.7 0 224 0H448C483.3 0 512 28.65 512 64V288C512 323.3 483.3 352 448 352H224z"/></svg>';
+    copy_btn.appendChild(copy_svg); 
+    copy_btn.appendChild(copy_text); 
+
+    copy_btn.onclick = function() {
+        navigator.clipboard.writeText(code[i].innerText)
+        .then(()=>{
+            // console.log('copy success!');
+            copy_text.textContent = 'copied!';
+            setTimeout(()=>{
+                copy_text.textContent = 'copy';
+            },2000);
+        }).catch((e)=>{
+            console.log(e);
+        })
+    }
+
+    code_info.appendChild(text);
+    code_info.appendChild(copy_btn);
+
+    code_block[i].appendChild(code_info);
+}
+
+// 初始化
+window.onscroll = tocTransfrom;
+tocTransfrom();
+
